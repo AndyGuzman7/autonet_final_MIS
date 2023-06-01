ea9e84b8fbb794e81e7bbe5920890140f03eecc6
AndyGuzman7 <andyguzman117@gmail.com>
2023-05-31 21:31:29 -0400

    crystal report functional

--- DAO.Implementacion/DataBase.cs
+++ DAO.Implementacion/DataBase.cs
@@ -13,7 +13,7 @@ namespace DAO.Implementacion
     {
         //Server=(local);Database=nombre_base_datos;Integrated Security=True;
 
-        private static string connectionString = @"Server=ANDYHP\SQLEXPRESS01;Database=BDDAUTONET;Integrated Security=True";
+        private static string connectionString = @"Server=AndyHP\MSSQLSERVER_PRIV;Database=BDDAUTONET2023;User Id=sa; Password=Univalle";
         //nikyprivate static string connectionString = @"Server=NIKY\SQLEXPRESS;Database=BDDAUTONET;User Id=autoNet;Password=Univalle;";
         //private static string connectionString = @"Server=NIKY\SQLEXPRESS;Database=BDDAUTONET;User Id=autoNet;Password=Univalle;";
 
